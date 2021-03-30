8f98d57ce9ba0ac2dfcaa1e0a7206cb85c475caa
Oriol Alfonso Riba <oriolalfonso91@gmail.com>
2021-03-30 17:46:05 +0200

    Simplify setup + reqs

--- setup.py
+++ setup.py
@@ -1,26 +1,5 @@
 from setuptools import setup, find_packages
 
-
-def read_requirements(filename):
-    with open(filename) as f:
-        reqs = f.readlines()
-    # filter comments and drop references to other requirements files
-    return [r.strip() for r in reqs
-            if r[0] != '#' and r[:4] != 'git+' and r[:2] != '-r']
-
-
-def read_dependency_links(filename):
-    with open(filename) as f:
-        reqs = f.readlines()
-    # filter comments and  drop references to other requirements files
-    return [r.strip() for r in reqs
-            if r[0] != '#' and r[:4] == 'git+' and r[:3] != '-r']
-
-
-install_reqs = read_requirements('./requirements.txt')
-dependency_links = read_dependency_links('./requirements.txt')
-
-
 setup(
     name='testb',
     version='1.0.0',
@@ -28,8 +7,6 @@ setup(
     author='Oriol Alfonso',
     author_email='oriolalfonso91@gmail.com',
     packages=find_packages(),
-    install_requires=install_reqs,
-    dependency_links=dependency_links,
-    data_files=[('.', ['requirements.txt', ])],
+    install_requires=['testa @ git+ssh://git@github.com/oalfonso-o/testa.git'],
     zip_safe=False,
 )
